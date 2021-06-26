952ccdb0f92cee19509fe052d5f246e8e03acbb7
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-06-26 01:21:06 +0000

    Using isEven to find out the even numbers

---
+++ even.js
@@ -0,0 +1,14 @@
+const isEven = function (num) {
+  return num % 2 === 0 ;
+}
+
+/* 
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11); 
+*/
+
+//We can use functions directly when we need them without signing them to a new variable. 
+
+console.log(isEven(10));
+console.log(isEven(11));
+
