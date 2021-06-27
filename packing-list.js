b500551037d406d216ed2dc3f376154591001be9
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-06-27 02:28:08 +0000

    Using a while loop instead of a for loop to iterate the same function

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,18 @@
 const packingList = ["bowls", "plate", "pots", "pans", "eating untensils", "glasses", "cups","cooking utensils"];
+
 console.log("Kitchen stuff to pack");
 
+let i = 0;
+
+//====USING A FOR LOOP===
+/*
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i])
+}
+*/
+
+//=====USING A WHILE LOOP TO DO THE SAME THING
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
