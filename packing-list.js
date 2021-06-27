1faf3f908907d4f9d77eb766d58fe0135e9dd8e4
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-06-27 02:21:00 +0000

    using a loop to go through each item if an array

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["bowls", "plate", "pots", "pans", "eating untensils", "glasses", "cups","cooking utensils"];
+console.log("Kitchen stuff to pack");
+
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i])
+}
